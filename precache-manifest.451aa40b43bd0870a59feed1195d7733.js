ffc6a95d0e90063fee6fda8e6f2cb518eed11a2c
Maryem Ben Hamouda <maryouma.msaken@gmail.com>
2020-02-27 15:59:40 +0100

    Updates

---
+++ precache-manifest.451aa40b43bd0870a59feed1195d7733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a1a0ac81d1f8b8a3887edeeee3b545",
+    "url": "/MaryemBenHamouda/nevbar-react/index.html"
+  },
+  {
+    "revision": "57d6e0a6d8db97ca0d24",
+    "url": "/MaryemBenHamouda/nevbar-react/static/css/2.4121280a.chunk.css"
+  },
+  {
+    "revision": "f1ca1a3c6c0fef898792",
+    "url": "/MaryemBenHamouda/nevbar-react/static/css/main.3756ce7d.chunk.css"
+  },
+  {
+    "revision": "57d6e0a6d8db97ca0d24",
+    "url": "/MaryemBenHamouda/nevbar-react/static/js/2.a1170d6f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MaryemBenHamouda/nevbar-react/static/js/2.a1170d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ca1a3c6c0fef898792",
+    "url": "/MaryemBenHamouda/nevbar-react/static/js/main.bbd2077d.chunk.js"
+  },
+  {
+    "revision": "ace448103ee8b4662add",
+    "url": "/MaryemBenHamouda/nevbar-react/static/js/runtime-main.6a5fdc00.js"
+  }
+]);
